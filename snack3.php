ba710e75466c99a9e31999fcc5290f475eed9f5c
LucaSpennato <luca.spennato@gmail.com>
2022-08-23 16:28:30 +0200

    numbers in lists items

--- snack3.php
+++ snack3.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    
     <title>Document</title>
 </head>
 <body>
-<h1>
+    <h1 class="ms-5 py-2">
         Snack 3
     </h1>
 
     <!-- Creare un array con 15 numeri casuali,
         tenendo conto che l'array non dovrà contenere 
         lo stesso numero più di una volta  -->
-
-    <h5>
         <?php
             $numbers = [];
 
@@ -24,12 +25,17 @@
                 $randNum = rand(1, 300);
 
                 if(!in_array($randNum, $numbers)){
-                    array_push($numbers, $randNum);
+                    $numbers[] = $randNum;
                 }
             }
-
-            var_dump($numbers);
         ?>
-    </h5>
+
+        <ul class="list-group w-50 ms-5">
+            <?php for ($i=0; $i < count($numbers); $i++) { ?>
+            <li class="list-group-item">
+                <?php echo $numbers[$i]; ?>
+            </li>
+            <?php } ?>
+        </ul>
 </body>
 </html>
